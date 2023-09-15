d531579570a4e3e1a559e73f0ee1a8f55f2606a3
Felipe-whitesoft <felipe.montana@whitesoft.com.au>
2023-09-14 23:27:55 -0500

    .next

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.node/,
-            use: 'raw-loader',
-        });
-
-        return config;
-    },
 }
 
 module.exports = nextConfig
